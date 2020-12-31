012041d90931529aecca3ecd8cd11036b888d2ff
BuildTools <unconfigured@null.spigotmc.org>
2020-12-31 01:26:34 +0000

    fix

--- stream.py
+++ stream.py
@@ -5,8 +5,8 @@ import json
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='AAAAAAAAAAAAAAAAAAAAAJFEKgEAAAAAUmp%2F8yloASSlyjrrRbV%2BYu3MBE0%3DvHDl6YnmRYk7tx6LIjDjBknIDzUtB48lafGXGwku12qdTvbspH'
-webhook_url_35P_tweet = 'https://discord.com/api/webhooks/794004355598385213/IJi3LhpDm5Z7M8pHuu1N13sAS_Tck6HOeKhyjUkmCLTDfsX8xQjt21q-I9nbTkcZeRCM'
-webhook_url_35P_Art = 'https://discord.com/api/webhooks/794011767999168522/enQP6wjcO0YwQe948mSPNMV7QCbJA5mhwOvVBKKEKcmZsR7-AMcI87BTQVRBEmMMnFUy'
+webhook_url_35P_tweet = 'https://discord.com/api/webhooks/786087345292181524/kE_jTEyXkaBh7ytk91cUOmcGv-3qNpUkVPvUeG_-akfX7uyUfEt_fLahrhAA7D0GnACV'
+webhook_url_35P_Art = 'https://discord.com/api/webhooks/786086350947876864/NHY2AowBSkI3xyzgxnQtMyeEcCIf87lGG6Be2xRZrGe911jif3RrI8l2jJTBHWvTru8_'
 webhook_url_Admin_twitter = 'https://discord.com/api/webhooks/794011967462834196/1gBCG4prIJ4sdIDqJjmREt2FN75GzXNij_OfNfW7_b8yURdh5WUcXQiRNojP-4BqgaXy'
 
 
