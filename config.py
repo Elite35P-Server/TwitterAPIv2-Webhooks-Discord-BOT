6dcf64aacc64a4471ab09fc2a5ab69ab80d4e4b9
noflm <61629565+noflm@users.noreply.github.com>
2021-04-13 20:23:33 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+#wh1
+wh1 = "Discord Webhook Link 1 Here"
+#wh2
+wh2 = "Discord Webhook Link 2 Here"
+#wh3
+wh3 = "Discord Webhook Link 3 Here"
+#wh4
+wh4 = "Discord Webhook Link 4 Here"
+#wh5
+wh5 = "Discord Webhook Link 5 Here"
+#Twitter API Token
+token = "Twitter API v2 Token Here"
